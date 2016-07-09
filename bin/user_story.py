8cecf9ab5dba673270979f8ea3fef5a63022d98f
= <jkearisman@wpi.edu>
2016-07-08 22:29:42 -0400

    "so that I" -> "so I" in user_story.py Another commit, I must admit Let's have another try

--- bin/user_story.py
+++ bin/user_story.py
@@ -2,7 +2,8 @@ import random
 import os
 
 fname_list = ["as_a.txt", "i_want.txt", "so_i.txt"]
-fpath_list = [ os.path.expanduser("~") + "/bin/user_stories/" + x for x in fname_list ]
+fpath_list = [ os.path.expanduser("~") + "/bin/user_stories/" + x
+        for x in fname_list ]
 
 file_list = [open(x, "r") for x in fpath_list]
 
@@ -11,5 +12,6 @@ my_choices = []
 for f in file_list:
     line_list = [l for l in f]
     my_choices.append( random.choice( line_list ).strip() )
-print( "As a {} I want {} so that I {}".format( my_choices[0], my_choices[1], my_choices[2] ))
+print( "As a {} I want {} so I {}".format(
+    my_choices[0], my_choices[1], my_choices[2] ))
 
