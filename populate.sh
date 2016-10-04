f2c0314064394580cd22895318fef3aa8fafceb2
= <jkearisman@wpi.edu>
2016-10-04 14:23:35 -0400

    Fixed populate script to delete existing config files and correctly move in configs

--- populate.sh
+++ populate.sh
@@ -3,9 +3,15 @@
 # Symlinks config files to their appropriate place in the home folder
 # If a file already exists (e.g. you already have a .zshrc),
 #  it will not clobber it
-ln -s ./vim $HOME/.vim
-ln -s ./xinitrc $HOME/.xinitrc
-ln -s ./bin $HOME/bin
-ln -s ./i3 $HOME/.i3
-ln -s ./vimperatorrc $HOME/.vimperatorrc
-ln -s ./zshrc $HOME/.zshrc
+rm -r $HOME/.vim
+ln -s $PWD/vim $HOME/.vim
+rm $HOME/.xinitrc
+ln -s $PWD/xinitrc $HOME/.xinitrc
+rm -r $HOME/bin
+ln -s $PWD/bin $HOME/bin
+rm -r $HOME/.i3
+ln -s $PWD/i3 $HOME/.i3
+rm $HOME/.vimperatorrc
+ln -s $PWD/vimperatorrc $HOME/.vimperatorrc
+rm $HOME/.zshrc
+ln -s $PWD/zshrc $HOME/.zshrc
