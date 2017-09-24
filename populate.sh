c46bb161ebd858038503247766e6c7dbec22df25
Jesse Earisman <jkearisman@wpi.edu>
2017-09-23 23:26:25 -0400

    Improved autoinstall script. Now installes packages and should new print at least 50% less junk

--- populate.sh
+++ populate.sh
@@ -1,17 +1,60 @@
 #!/bin/bash
 
-# Symlinks config files to their appropriate place in the home folder
-# If a file already exists (e.g. you already have a .zshrc),
-#  it will not clobber it
-rm -r $HOME/.vim
+sudo pacman -S $(cat packages.txt) --noconfirm
+
+
+#Setting up pacaur (cower and expac are dependancies)
+sudo pacman -S expac --noconfirm
+git clone https://aur.archlinux.org/cower.git
+cd cower
+makepkg -i --noconfirm
+cd ..
+rm -rf cower
+git clone https://aur.archlinux.org/pacaur.git
+cd pacaur
+makepkg -i --noconfirm
+cd ..
+rm -rf cower
+
+
+
+function mkdir_if_dne {
+	if [[ ! -d $1 ]]
+	then
+		mkdir $1
+	fi
+}
+
+function rm_if_e {
+	if [[ -e $1 || -d $1 ]]
+	then
+		rm -r $1
+	fi
+}
+
+#vim
+rm_if_e $HOME/.vim
 ln -s $PWD/vim $HOME/.vim
-rm $HOME/.xinitrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+
+rm_if_e $HOME/.xinitrc
 ln -s $PWD/xinitrc $HOME/.xinitrc
-rm -r $HOME/bin
+
+rm_if_e $HOME/bin
 ln -s $PWD/bin $HOME/bin
-rm -r $HOME/.i3
+
+rm_if_e $HOME/.i3
 ln -s $PWD/i3 $HOME/.i3
-rm $HOME/.vimperatorrc
+
+rm_if_e $HOME/.vimperatorrc
 ln -s $PWD/vimperatorrc $HOME/.vimperatorrc
-rm $HOME/.zshrc
+
+rm_if_e $HOME/.zshrc
 ln -s $PWD/zshrc $HOME/.zshrc
+
+mkdir_if_dns $HOME/.config
+mkdir_if_dne $HOME/.config/lilyterm
+rm_if_e $HOME/.config/lilyterm/default.conf
+ln -s $PWD/lilyterm/default.conf $HOME/.config/lilyterm/default.conf
